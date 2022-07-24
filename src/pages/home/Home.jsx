a60b4befa6d03288927dd930d3ba48a63f73f2f5
its-kaushik <mkaushik0033@gmail.com>
2022-07-24 17:00:13 +0530

    Adding page components and their styles.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './home.scss' ;
+
+const Home = () => {
+    return (
+        <div className="home" >
+            <h1 className='title'>mayank kaushik</h1>
+        </div>
+    )
+}
+
+export default Home ;
