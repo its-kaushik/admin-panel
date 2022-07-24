3caf90e08a7fde76f349686de9a4998759793365
its-kaushik <mkaushik0033@gmail.com>
2022-07-24 17:31:47 +0530

    making the sidebar and navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import Sidebar from '../../components/sidebar/Sidebar' ;
 import './home.scss' ;
 
 const Home = () => {
     return (
-        <div>
-            home
+        <div className="home" >
+            <Sidebar />
+            <div className='homeContainer' >container</div>
         </div>
     )
 }
